24f63157237ea178b51821993ccd5e1fc83b1be3
Victoria Ilyina <victoria.ilyina@progmax.net>
2026-05-03 22:55:46 +0700

    feat: change maps source api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,44 +1,40 @@
+/* global ymaps */
 import React, { useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 import './Map.scss';
 
-import { MAP_CONSTANTS } from '../../constants';
-
 import getCoordinatesFromAddress from './Map.utils';
-
-mapboxgl.accessToken = MAP_CONSTANTS.MAP_BOX_TOKEN;
 
 const Map = ({ address }) => {
   const mapContainerRef = useRef(null);
-  const [lat, setLatitude] = useState(MAP_CONSTANTS.GLOBAL_COORDS);
-  const [long, setLongitude] = useState(MAP_CONSTANTS.GLOBAL_COORDS);
-
-  getCoordinatesFromAddress(address).then((data) => {
-    if (data) {
-      const [longitude, latitude] = data.split(' ');
-      setLatitude(latitude);
-      setLongitude(longitude);
-    }
-  });
+  const [lat, setLatitude] = useState(0);
+  const [long, setLongitude] = useState(0);
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [long, lat],
-      zoom: MAP_CONSTANTS.MAP_ZOOM,
+    getCoordinatesFromAddress(address).then((data) => {
+      if (data) {
+        const [longitude, latitude] = data.split(' ');
+        setLatitude(parseFloat(latitude));
+        setLongitude(parseFloat(longitude));
+      }
     });
+  }, [address]);
 
-    new mapboxgl.Marker().setLngLat([long, lat]).addTo(map);
+  useEffect(() => {
+    if (lat !== 0 && long !== 0) {
+      const map = new ymaps.Map(mapContainerRef.current, {
+        center: [lat, long],
+        zoom: 15,
+      });
 
-    return () => map.remove();
+      const marker = new ymaps.Placemark([lat, long]);
+      map.geoObjects.add(marker);
+
+      return () => map.destroy();
+    }
   }, [lat, long]);
 
-  return (
-    <div className="Map" ref={mapContainerRef} />
-  )
+  return <div className="Map" ref={mapContainerRef} />;
 }
 
 export default Map;
--- src/constants/map-constant.js
+++ src/constants/map-constant.js
@@ -1,8 +1,6 @@
 export default {
-  GLOBAL_COORDS: 0,
-  MAP_ZOOM: 12.5,
   MAP_BOX_TOKEN:
     'pk.eyJ1Ijoic3RhY2V5c3ljaCIsImEiOiJja2FpaGw0N2YwMHUzMzRtaXNtazBtYXpkIn0.DPP70YMFkqRuAScBuq5_Gw',
     YANDEX_TOKEN:
-  'https://geocode-maps.yandex.ru/1.x/?apikey=8a4327a5-b906-4828-a81a-88e55dd3039e&format=json&lang=ru_RU&geocode=',
+  'https://geocode-maps.yandex.ru/1.x/?apikey=a919cc7d-63a5-4061-a32d-d2d6cc97d1d2&format=json&lang=ru_RU&geocode=',
 };
